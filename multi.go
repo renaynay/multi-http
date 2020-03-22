1c67b3de97e614f9f49bddbfe8fd7382c3b068e5
renaynay <41963722+renaynay@users.noreply.github.com>
2020-03-22 23:34:54 +0100

    reformat server, plus add ws client to send messages

--- multi.go
+++ multi.go
@@ -2,18 +2,70 @@ package main
 
 import (
 	"net/http"
+	"strings"
 
-	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
+	log "github.com/sirupsen/logrus"
 )
 
-func main() {
-	router := mux.NewRouter()
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("websocket!"))
-	}).Headers("Connection", "upgrade", "Upgrade", "websocket", "Sec-Websocket-Version", "13")
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("just plain http!"))
-	})
+type Callback func(conn *websocket.Conn) error
 
-	http.ListenAndServe(":8080", router)
+func main() {
+	Listen()
+}
+
+func Listen() {
+
+	http.Handle("/", RouteWSOrHTTP())
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func IsWebsocket(r *http.Request) bool {
+	return strings.ToLower(r.Header.Get("Upgrade")) == "websocket" && strings.ToLower(r.Header.Get("Connection")) == "upgrade"
+}
+
+func RouteWSOrHTTP() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if IsWebsocket(r) {
+			HandleWebsocket(w, r)
+			return
+		}
+
+		// call HTTP-specific handler
+
+		w.Write([]byte("HTTP!"))
+	})
+}
+
+
+func HandleWebsocket(w http.ResponseWriter, r *http.Request) {
+	// upgrader is needed to upgrade the HTTP Connection to a websocket Connection
+	upgrader := &websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
+
+	//Upgrading HTTP Connection to websocket connection
+	wsConn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Printf("error upgrading %s", err)
+		return
+	}
+
+	for {
+		_, raw, err := wsConn.ReadMessage()
+		if err != nil {
+			log.Error("error reading message from ws connection: ", err)
+			return
+		}
+
+		log.Info("message received: ", string(raw))
+
+		err = wsConn.WriteMessage(websocket.TextMessage, ([]byte("message received on websocket connection!")))
+		if err != nil {
+			log.Error("error writing message to ws connection: ", err)
+			return
+		}
+	}
 }
